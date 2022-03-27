2f9d4dfe68a7b1ac393bcb44189d3ea8d0fbb6a7
expshashi <97669486+expshashi@users.noreply.github.com>
2022-03-28 01:46:43 +0530

    Add files via upload

---
+++ equipment/Book.java
@@ -0,0 +1,6 @@
+package equipment;
+
+public class Book
+{
+
+}
---
+++ equipment/Equipment.java
@@ -0,0 +1,10 @@
+package equipment;
+
+public class Equipment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
